fdb938d0b5be69eb4353db6dafaae49f977aaf9b
iristhepianist <164552050+iristhepianist@users.noreply.github.com>
2024-11-29 21:26:05 +0530

    Add files via upload

--- groovy/postInit/eio/eiorep.groovy
+++ groovy/postInit/eio/eiorep.groovy
@@ -2,4 +2,4 @@
 recipemap('arc_furnace').recipeBuilder()
         .inputs(ore('blockQuartz') * 4)
         .outputs(item('enderio:block_fused_quartz'))
-        .duration(20).EUt(16).buildAndRegister()
+        .duration(20).EUt(16).buildAndRegister() 
