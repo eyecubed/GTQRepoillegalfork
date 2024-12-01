0b234dd8397cb798d91e5b7d6320065712720900
iristhepianist <164552050+iristhepianist@users.noreply.github.com>
2024-12-01 17:54:18 +0530

    Add files via upload

---
+++ groovy/postInit/magic/root.groovy
@@ -0,0 +1,42 @@
+mods.roots.fey_crafter.recipeBuilder()
+    .name('gay_sex')
+    .input(item('minecraft:stick'),item('minecraft:stick'),ore('gemAmethyst'),ore('gemAmethyst'),ore('gemAmethyst')) // Must be exactly 5
+    .output(item('mysticalworld:amethyst_pickaxe'))
+    .xp(25)
+    .register()
+
+mods.roots.fey_crafter.recipeBuilder()
+    .name('gay_sex1')
+    .input(item('minecraft:stick'),item('minecraft:stick'),ore('gemAmethyst'),ore('gemAmethyst'),ore('gemAmethyst')) // Must be exactly 5
+    .output(item('mysticalworld:amethyst_axe'))
+    .xp(25)
+    .register()
+
+mods.roots.fey_crafter.recipeBuilder()
+    .name('gay_sex2')
+    .input(item('minecraft:stick'),item('minecraft:stick'),ore('vine'),ore('gemAmethyst'),ore('gemAmethyst')) // Must be exactly 5
+    .output(item('mysticalworld:amethyst_hoe'))
+    .xp(25)
+    .register()
+
+mods.roots.fey_crafter.recipeBuilder()
+    .name('gay_sex3')
+    .input(item('minecraft:stick'),item('minecraft:stick'),ore('vine'),ore('gemAmethyst'),ore('gemAmethyst')) // Must be exactly 5
+    .output(item('mysticalworld:amethyst_sword'))
+    .xp(25)
+    .register()
+
+mods.roots.fey_crafter.recipeBuilder()
+    .name('gay_sex4')
+    .input(item('minecraft:stick'),item('minecraft:stick'),ore('vine'),ore('vine'),ore('gemAmethyst')) // Must be exactly 5
+    .output(item('mysticalworld:amethyst_shovel'))
+    .xp(25)
+    .register()
+
+
+mods.roots.fey_crafter.recipeBuilder()
+    .name('say_gex')
+    .input(ore('feyLeather'),item('roots:runic_dust'),ore('runedObsidian'),ore('runedObsidian'),ore('runedObsidian')) // Must be exactly 5
+    .output(item('dimensionaledibles:nether_cake'))
+    .xp(100)
+    .register()
