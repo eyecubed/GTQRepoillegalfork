bf12fa223a1466dc2caee6886e4880e374e472dc
iristhepianist <164552050+iristhepianist@users.noreply.github.com>
2025-05-25 20:47:44 +0530

    Delete postInit directory

--- postInit/thermal/tamixer.groovy
+++
@@ -1,6 +0,0 @@
-
-recipemap('mixer').recipeBuilder()
-        .inputs(ore('dustSilicon'))
-        .inputs(ore('dustRedstone') * 4)
-        .outputs(item('gregtech:meta_dust', 32145))
-        .duration(220).EUt(32).buildAndRegister()
